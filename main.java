4ffd0da6b8a4c29c75bb0cc77f36ab9cf8bb67e2
kamsiEmekaOkeke <kamsi.emeka-okeke@binghamuni.edu.ng>
2024-06-11 13:48:12 +0100

    main.java

---
+++ main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Stack myStack = new Stack(5);
+        myStack.push(10);
+        myStack.push(30);
+        myStack.printStack();
+    }   
+}
